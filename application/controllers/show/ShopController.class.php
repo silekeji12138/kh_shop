6759f1a034c9946361f433e0ede081bb505a881f
JoeRowe123 <917315039@qq.com>
2018-01-10 18:53:48 +0800

    出售网店

---
+++ application/controllers/show/ShopController.class.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: DELL
+ * Date: 2018/1/9
+ * Time: 17:01
+ */
+class ShopController extends BaseController
+{
+    /**
+     * 卖家
+     */
+    //要出售淘宝店
+    public function taobaoAction(){
+        $request = $_SERVER['REQUEST_METHOD'];
+        if ($request=='POST'){
+            $model = new Model('shangpin');
+            $data = $_POST;
+            $data['uid'] = $_SESSION['admin']['user_id'];
+            $data['szpt'] = "淘宝网店";
+            $data['wgkf'] = $data['yb']."-".$data['yz']."-".$data['wg'];
+            $data['zj'] = $data['sfz']."-".$data['scsfz']."-".$data['hkb']."-".$data['schkb'];
+            if($model->insert($data)){
+                return $this->jump('index.php?p=show&c=shop&a=taobao','提交成功',3);
+            }else{
+                return $this->jump('index.php?p=show&c=shop&a=taobao','提交失败',3);
+            }
+
+        }
+        include CUR_VIEW_PATH . "Sshop" . DS ."taobao.html";
+    }
+
+    //出售天猫
+    public function tianmaoAction(){
+        $request = $_SERVER['REQUEST_METHOD'];
+        if ($request=='POST'){
+            $model = new Model('shangpin');
+            $data = $_POST;
+            $data['uid'] = $_SESSION['admin']['user_id'];
+            $data['szpt'] = "天猫商城";
+            $data['wgkf'] = $data['yb']."-".$data['yz']."-".$data['wg'];
+            $data['zj'] = $data['sfz']."-".$data['scsfz']."-".$data['hkb']."-".$data['schkb'];
+            if($model->insert($data)){
+                return $this->jump('index.php?p=show&c=shop&a=tianmao','提交成功',3);
+            }else{
+                return $this->jump('index.php?p=show&c=shop&a=tianmao','提交失败',3);
+            }
+
+        }
+        include CUR_VIEW_PATH . "Sshop" . DS ."tianmao.html";
+    }
+
+    //出售的网店
+    public function shopSaleAction(){
+        $model = new Model('sale');
+        $uid = $_SESSION['admin']['user_id'];
+        $lists = $model->select("select *from sl_sale WHERE uid={$uid}");
+        include CUR_VIEW_PATH . "Sshop" . DS ."shop_sale.html";
+    }
+
+    /**
+     * 买家
+     */
+    //购买的网店
+    public function buyAction(){
+        $model = new Model('buy');
+        $uid = $_SESSION['admin']['user_id'];
+        $lists = $model->select("select *from sl_buy WHERE uid={$uid}");
+        include CUR_VIEW_PATH . "Sshop" . DS ."buy.html";
+    }
+
+
+    //收藏的网店
+    public function saveAction(){
+
+        include CUR_VIEW_PATH . "Sshop" . DS ."save.html";
+    }
+
+    //竞拍的网店
+    public function auctionAction(){
+
+        include CUR_VIEW_PATH . "Sshop" . DS ."auction.html";
+    }
+
+    /**
+     * 删除记录  通用
+     * param $model  表明
+     * param $id     id
+     */
+    public function delAction(){
+        $request = $_SERVER['REQUEST_METHOD'];
+        $table = $_GET['model'];
+        $model = new Model("$table");
+        if ($request=='POST'){
+            $ids = $_POST['ids'];
+        }else{
+            $ids = $_GET['id'];
+        }
+        if($model->delete($ids)){
+            return $this->jump($_SERVER['HTTP_REFERER'],'删除成功',3);
+        }else{
+            return $this->jump($_SERVER['HTTP_REFERER'],'删除失败',3);
+        }
+    }
+}
--- application/controllers/show/UserController.class.php
+++ application/controllers/show/UserController.class.php
@@ -23,5 +23,106 @@ class UserController extends BaseController
         session_destroy();
         $this->jump('index.php?p=show&c=login&a=login','',0);
     }
-    
+
+    //充值
+    public function rechargeAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."recharge.html";
+    }
+
+    //充值记录
+    public function recordAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."record.html";
+    }
+
+    //提现
+    public function cashAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."cash.html";
+    }
+
+    //提现记录
+    public function logAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."log.html";
+    }
+
+    //收支
+    public function paymentAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."payment.html";
+    }
+
+    //冻结明细
+    public function frozenAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."frozen.html";
+    }
+
+    //个人资料
+    public function personAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."person.html";
+    }
+
+    //登录密码
+    public function passwordAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."password.html";
+    }
+
+    //支付密码
+    public function paywordsAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."paywords.html";
+    }
+
+    //密码保护
+    public function protectAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."protect.html";
+    }
+
+    //手机认证
+    public function phoneAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."phone.html";
+    }
+
+    //邮箱认证
+    public function emailAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."email.html";
+    }
+
+    //账号安全
+    public function safeAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."safe.html";
+    }
+
+    //建议
+    public function adviceAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."advice.html";
+    }
+
+    //咨询
+    public function consultAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."consult.html";
+    }
+
+    //投诉
+    public function complaintAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."complaint.html";
+    }
+
+    //评价
+    public function assessAction(){
+
+        include CUR_VIEW_PATH . "Suser" . DS ."assess.html";
+    }
 }
